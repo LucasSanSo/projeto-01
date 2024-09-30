f2ac7554facdc76a2e2811d3ae911116b0c8c624
LucasSanSo <namikazefabi@gmail.com>
2024-09-30 19:26:07 -0300

    Update page.tsx

--- projeto-01/src/app/page.tsx
+++ projeto-01/src/app/page.tsx
@@ -1,27 +1,66 @@
-import { useEffect, useState } from "react";
+"use client"
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { FiTrash, FiEdit, FiCheck } from "react-icons/fi";
-import {api} from "./api";
+import { api } from "./api";
+import { Asul } from "next/font/google";
 
-interface TaskProps{
+interface TaskProps {
   id: string;
   description: string;
   date: string;
-  status:boolean;
+  status: boolean;
 }
 
-const [tasks, setTasks] = useState<TaskProps[]>([])
 
-useEffect(() => {
-  loadTasks();
-})
-
-async function loadTasks() {
-  const response = await api.get("/task")
-  setTasks(response.data)
-   
-  
-}
 export default function Home() {
+  const [tasks, setTasks] = useState<TaskProps[]>([])
+
+  const descriptionRef = useRef<HTMLInputElement | null>(null)
+
+  const dataRef = useRef<HTMLInputElement | null>(null)
+
+
+  useEffect(() => {
+    readTasks();
+  })
+  async function readTasks() {
+    const response = await api.get("/task")
+  }
+
+  async function createTask(event: FormEvent) {
+    event.preventDefault()
+    const response = await api.post("/task", {
+      description: descriptionRef.current?.value,
+      date: dataRef.current?.value
+    })
+
+    setTasks(allTasks => [...allTasks, response.data])
+  }
+
+  async function deleteTask(id: string) {
+    try {
+      await api.delete("/task", {
+        params: {
+          id: id
+        }
+      })
+      const allTasks = tasks.filter((task) => task.id !== id)
+      setTasks(allTasks)
+
+    } catch (err) {
+      alert(err)
+    }
+
+  }
+  async function setTaskDone(id: String) {
+    try {
+      const response = await api.put("/task", { params: { id: id }, status: true })
+      setTasks(response.data)
+    }
+
+    catch (err) { alert(err) }
+
+  }
   return (
     <div className="w-full min-h-screen bg-rose-950 flex 
     justify-center px-4">
@@ -31,40 +70,36 @@ export default function Home() {
           font-medium text-center">
             Lista de tarefas
           </h1>
-          <form className="flex flex-col my-6">
+          <form className="flex flex-col my-6" onSubmit={createTask}>
             <label className="text-slate-200">Descrição tarefa</label>
-            <input type="text" className="w-full mb-5 p-2 rounded" />
+            <input type="text" className="w-full mb-5 p-2 rounded" ref={descriptionRef} />
             <label className="text-slate-200">Data</label>
-            <input type="date" className="w-full mb-5 p-2 rounded" />
+            <input type="date" className="w-full mb-5 p-2 rounded" ref={dataRef} />
             <input type="submit" value={"Adicionar"} className="cursor-pointer w-full bg-slate-800 font-medium text-slate-200 p-4" />
           </form>
         </section>
 
         <section className="mt-5 flex flex-col">
-       {tasks.map((task)=> 
-     <article
-     className="w-full bg-slate-200 text-slate-800 p-2 rounded
-   relative hover:bg-sky-300">
-     <p>{task.description}</p>
-     <p>{task.date}</p>
-     <p>{task.status}</p>
-     <button
-       className="flex absolute right-14 -top-2 bg-green-400 w-7 h-7 
-       items-center justify-center text-slate-200"> <FiCheck></FiCheck></button>
+          {tasks.map((task) =>
+            <article
+              className="w-full bg-slate-200 text-slate-800 p-2 rounded
+   relative hover:bg-sky-300" key={task.id}>
+              <p>{task.description}</p>
+              <p>{task.date}</p>
+              <p>{task.status}</p>
+              <button
+                className="flex absolute right-14 -top-2 bg-green-400 w-7 h-7 
+       items-center justify-center text-slate-200" onClick={() => setTaskDone(task.id)}> <FiCheck></FiCheck></button>
 
-     <button
-       className="flex absolute right-7 -top-2 bg-yellow-400 w-7 h-7 
-       items-center justify-center text-slate-200"> <FiEdit></FiEdit></button>
-
-<button
-       className="flex absolute right-0 -top-2 bg-red-400 w-7 h-7 
-       items-center justify-center text-slate-200"> <FiTrash></FiTrash></button>
+              <button
+                className="flex absolute right-0 -top-2 bg-red-400 w-7 h-7 
+       items-center justify-center text-slate-200" onClick={() => deleteTask(task.id)}> <FiTrash></FiTrash></button>
 
 
-   </article>
-       )}
-       
-         
+            </article>
+          )}
+
+
         </section>
       </main>
     </div>
